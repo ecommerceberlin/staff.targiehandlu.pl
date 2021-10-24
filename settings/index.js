84ca52bd6f8605a6a5cabb847b5030ca3c0f88fd
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-24 15:51:04 +0200

    resolve api by env vars

--- settings/index.js
+++ settings/index.js
@@ -7,7 +7,7 @@ const settings =  {
             'admin': '1751bfe48d5ad21fd9d'
         },
         lang_api_endpoint : 'https://api.eventjuicer.com/proxy?url=https%3A%2F%2Flocalise.biz%2Fapi%2Fexport%2Fall.json%3Fformat%3Dmulti%26pretty%26key%3DSHiwxgKaPMx_KThQH2zcdzwiKEMzuNBm',    
-        api : "https://api.eventjuicer.com/v1/public/hosts/targiehandlu.pl",
+        api : "https://api.eventjuicer.com/v1/public/hosts/" . process.env.NEXT_PUBLIC_PROJECT,
         proxy: "https://api.eventjuicer.workers.dev?url=",
         // api : "http://eventjuicer-api.test/v1/public/hosts/targiehandlu.pl",
         post_api : "https://api.eventjuicer.com/v1/public/hosts/targiehandlu.pl/register",
@@ -64,30 +64,19 @@ const settings =  {
    
     common : {
 
-        organizer_name : 'Infoguru Sp. z o.o. Sp. k.',
-        organizer_address : 'POLAND, Poznań, Truskawiecka 13',
-        organizer_regno : 'VAT ID 7811967834',
-        event_name : 'Targi eHandlu',
-        event_location : 'EXPO XXI Warszawa, Prądzyńskiego 12/14',
-        event_date : '20 października  2021',
-        event_hours : '10:00-17:00',
+        organizer_name : '',
+        organizer_address : '',
+        organizer_regno : '',
+        event_name : process.env.NEXT_PUBLIC_PROJECT,
+        event_location : '',
+        event_date : '',
+        event_hours : '',
     },
 
     sales_support : {
         title: 'event.support.hello',
         description: 'event.support.description',
-        people : [
-            {             
-                name: 'Karolina Michalak',
-                position : 'Relationship Manager',
-                langs : ["pl","en"],
-                avatar: 'https://res.cloudinary.com/eventjuicer/image/upload/v1598009850/targiehandlu_people_km.jpg',
-                phone: '+48 721 945 134',
-                email: 'karolina.michalak@targiehandlu.pl',
-                chatlio : true
-            },
-          
-          ]
+        people : []
     },
 
    
