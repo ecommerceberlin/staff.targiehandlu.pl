53d80d69d1a9775c0c7ec71749cc73d0aeff8801
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-15 12:25:34 +0100

    added agenda

---
+++ pages/agenda.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import {
+  connect,
+  reduxWrapper,
+  configure,
+  Wrapper,
+
+} from 'eventjuicer-site-components';
+
+import {
+  WidgetPresentersList
+} from 'eventjuicer-admin-site-components'
+
+
+
+const settings = require('../settings').default;
+
+
+
+const PageMap = () => (<Wrapper first><WidgetPresentersList /></Wrapper>)
+
+
+
+export const getStaticProps = reduxWrapper.getStaticProps(async (props) => {
+  return await configure(props, {
+    settings : settings,
+    preload : ["presenters"]
+  })
+})
+
+export default connect()(PageMap);
